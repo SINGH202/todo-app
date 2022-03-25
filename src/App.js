ad8558185bbd1b94864f0741bc362eaee0820332
ANURAG SINGH <adarshsaurav6613@gmail.com>
2022-03-26 02:01:45 +0530

    Added every thing

---
+++ src/App.js
@@ -0,0 +1,159 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+const axios = require("axios");
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [status, seStatus] = useState(false);
+  const [list, setList] = useState([]);
+  let n = 1;
+  const api = `https://json-todo-server.herokuapp.com/todo`;
+
+  const getData = () => {
+    fetch(`${api}`)
+      .then((d) => d.json())
+      .then((res) => {
+        setList(res);
+      });
+  };
+  const postData = () => {
+    const data = {
+      title,
+      date,
+      time,
+      status,
+    };
+    if (data.title !== "" && data.date !== "" && data.time !== "") {
+      // console.log(data)
+      axios({
+        method: "post",
+        url: api,
+        data,
+      })
+        .then((res) => {
+          // console.log(res.data)
+        })
+        .then(getData);
+    } else {
+      alert("Fill all feilds");
+    }
+  };
+
+  const removeComplete = (e) => {
+    if (e.statue == true) {
+      axios.delete(api + "/" + e.id);
+    }
+  };
+
+  const deleteAll = () => {
+    setList(list.filter(removeComplete));
+    getData();
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <input type="text" name="title" id="title" onChange={handleChange} />
+        <br />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          type="time"
+          name="time"
+          id="time"
+          onChange={(e) => {
+            setTime(e.target.value);
+          }}
+        />
+        <br />
+        <button
+          onClick={
+            postData
+            // console.log(title, date, time, status)
+          }
+        >
+          Add
+        </button>
+
+        <br />
+        <button onClick={deleteAll}>Delete all complete Tasks</button>
+        {/* <input type="checkbox"  onChange={(e) => seStatus(!status)}/> */}
+        {/* <input
+    type=checkbox"
+    value="check"
+    onChange={(e) => this.setState({check: !check.value})}
+  />  */}
+      </div>
+      <div>
+        <table id="table">
+          <thead>
+            <tr>
+              <td>Index</td>
+              <td>Title</td>
+              <td>Time</td>
+              <td>Date</td>
+              <td>Status</td>
+              <td>Actions</td>
+              <td>Delete Task</td>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((e) => {
+              return (
+                <tr key={e.id}>
+                  <td>{n++}</td>
+                  <td>{e.title}</td>
+                  <td>{e.time}</td>
+                  <td>{e.date}</td>
+                  <td>{e.statue ? "Complete" : "Incomplete"}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        axios
+                          .patch(api + "/" + e.id, {
+                            statue: !e.statue,
+                          })
+                          .catch((error) => console.log(error))
+                          .then(getData);
+                      }}
+                    >
+                      Change
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        axios.delete(api + "/" + e.id).then(getData);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                  {/* <td>{e.gamerating}</td> */}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
