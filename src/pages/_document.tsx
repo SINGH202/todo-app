e4f9762b136579d5b7809aff17506f988dd2de64
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-07-24 13:30:59 +0530

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="/assets/web-icon.png" />
       <body>
         <Main />
         <NextScript />
